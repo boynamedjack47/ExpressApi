98a9ee23abe74f33107313f6aa8ff21925d23724
Jack Barker <boynamedjack@Jacks-MacBook-Pro.local>
2025-01-12 14:19:28 -0700

    Updating index.js to be entry point for app

--- index.js
+++ index.js
@@ -15,3 +15,15 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+const authRoutes = require('./routes/authRoutes');
+
+// Use the authentication routes
+app.use('/auth', authRoutes);
+
+
+const cardRoutes = require('./routes/cardRoutes');
+
+// Use the card management routes
+app.use('/cards', cardRoutes);
+
